2fb993667eae58896f4ed089f1743865cc2cd49f
Phong <phongtrandangphong@gmail.com>
2017-03-10 03:54:41 +0700

    CRUD user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,6 @@ require "open-uri"
 
 class UsersController < ApplicationController
   def data
-  end
-
-  def index
     require "open-uri"
     require 'json'
     # Get data from url
@@ -26,6 +23,53 @@ class UsersController < ApplicationController
         new_user.save
       end
     end
+    redirect_to users_path
+  end
+
+  def index
     @users = User.paginate(:page => params[:page], :per_page => 10)
   end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :first, :last, :address, :balance)
+    end
+
 end
